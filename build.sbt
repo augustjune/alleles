313749b025385cb3ecf0f956a0033ed840a1c2e1
empi <jurij.jurich@gmail.com>
2018-09-07 19:50:25 +0200

    Added type declarations for the skeleton

--- build.sbt
+++ build.sbt
@@ -3,3 +3,8 @@ name := "allele"
 version := "0.1"
 
 scalaVersion := "2.12.6"
+
+libraryDependencies ++= Seq (
+  "org.scalactic" %% "scalactic" % "3.0.5",
+  "org.scalatest" %% "scalatest" % "3.0.5" % "test"
+)
---
+++ src/main/scala/genetic/package.scala
@@ -0,0 +1,25 @@
+import scala.util.Random
+
+package object genetic {
+  implicit class RandomExtension(rand: Random) {
+    def shot(chance: Double): Boolean = rand.nextDouble() < chance
+
+    //ToDo - rename method
+    def shot(chances: Seq[Double]): Int = {
+      def iter(chances: List[Double], i: Int, shot: Double): Int = chances match {
+        case Nil => throw new RuntimeException
+        case h :: t => if (shot <= h) i else iter(t, i + 1, shot - h)
+      }
+      iter(chances.sorted.reverse.toList, 0, Random.nextDouble())
+    }
+  }
+
+  type Population = List[Permutation]
+
+  //ToDo - implement
+  class Permutation() {
+    def fitnessValue: Int = ???
+    def mutate: Permutation = ???
+    def randomCrossOver(other: Permutation): Permutation = ???
+  }
+}
