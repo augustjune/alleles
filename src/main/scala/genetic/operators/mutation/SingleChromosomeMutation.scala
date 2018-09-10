b8b77de3c2222824f388944606ab636f36f10504
empi <jurij.jurich@gmail.com>
2018-09-10 21:34:49 +0200

    Optimized some operators

--- src/main/scala/genetic/operators/mutation/SingleChromosomeMutation.scala
+++ src/main/scala/genetic/operators/mutation/SingleChromosomeMutation.scala
@@ -6,8 +6,8 @@ import genetic.operators.Mutation
 import scala.util.Random
 
 case class SingleChromosomeMutation[A](chance: Double)(implicit m: Mutator[A]) extends Mutation[A] {
-  def apply(population: Population[A]): Population[A] = population.map(
-    if (Random.shot(chance)) m.mutate else identity)
+  def apply(population: Population[A]): Population[A] = population.map(a =>
+    if (Random.shot(chance)) m.mutate(a) else a)
 
   override def toString: String = s"SingleChromosomeMutation of chance $chance"
 }
--- src/main/scala/genetic/operators/selection/Tournament.scala
+++ src/main/scala/genetic/operators/selection/Tournament.scala
@@ -6,19 +6,8 @@ import genetic.{Fitness, Population}
 import scala.util.Random
 
 case class Tournament[A](size: Int)(implicit f: Fitness[A]) extends Selection[A] {
-  def apply(population: Population[A]): Population[A] = {
-    def takeBest(tour: Population[A]): A = tour.minBy(f.value)
-
-    def selectOne: A =
-      takeBest(Random.shuffle(population).take(size))
-
-    def selectUntil(newPopSize: Int, acc: List[A]): List[A] =
-      if (acc.length < newPopSize)
-        selectUntil(newPopSize, selectOne :: acc)
-      else acc
-
-    selectUntil(population.length, Nil)
-  }
+  def apply(population: Population[A]): Population[A] = population.map(_ =>
+    Random.shuffle(population).take(size).minBy(f.value))
 
   override def toString: String = s"Tournament selection with tournament size of $size"
 }
