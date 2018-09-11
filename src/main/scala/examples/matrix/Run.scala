001cedb58abf1bb5e88f48db00fc9b29b6a8081b
empi <jurij.jurich@gmail.com>
2018-09-11 19:03:17 +0200

    Added concurrent execution to matrix example

--- src/main/scala/examples/matrix/Run.scala
+++ src/main/scala/examples/matrix/Run.scala
@@ -2,33 +2,42 @@ package examples.matrix
 
 
 import cats.kernel.Semigroup
-
-import concurrent.duration._
-import genetic.{AlgoSettings, GeneticAlgorithm, Population}
 import examples.matrix.matrices.{FlowMatrix, MatrixSource, RangeMatrix}
+import genetic._
 import genetic.genotype._
+import genetic.genotype.syntax._
 import genetic.operators.mixing.ClassicCrossover
 import genetic.operators.mutation.ComplexMutation
-import genetic.operators.selection.{Roulette, Tournament}
-import genetic.genotype.syntax._
+import genetic.operators.selection.Tournament
 
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration._
+import scala.concurrent.{Await, Future}
 import scala.language.postfixOps
 
 object Run extends App {
-  val (flow, range): (FlowMatrix, RangeMatrix) = new MatrixSource("/home/jura/Projects/jslinkin/allele/src/main/resources/matrix_16.txt").toMatrices
+  val (flow, range): (FlowMatrix, RangeMatrix) = new MatrixSource("http://anjos.mgi.polymtl.ca/qaplib/data.d/had20.dat").toMatrices
 
   implicit val fitness: Fitness[Permutation] = Permutation.fitness(range, flow)
   implicit val combinator: Semigroup[Permutation] = (perm1: Permutation, perm2: Permutation) => perm1.crossover(perm2)
   implicit val mutator: RandomChange[Permutation] = (perm: Permutation) => perm.mutate
   implicit val source: Design[Permutation] = Design.pure(() => Permutation.create(flow, range))
 
-  val settings = AlgoSettings[Permutation](100, Tournament(20), ClassicCrossover(0.5), ComplexMutation(0.2, 0.7))
-  val evolved: Population[Permutation] = GeneticAlgorithm.iterate(settings, 3 seconds)
+  val settings = AlgoSettings[Permutation](100, Tournament(20), ClassicCrossover(0.25), ComplexMutation(0.3, 0.7))
 
-  def minFitness(population: Population[Permutation]): Int = population.map(_.fitness).min
+  def evolve(settings: AlgoSettings[Permutation]): Future[Permutation] = Future {
+    GeneticAlgorithm.iterate(settings, 5 seconds).best
+  }
 
-  println(s"Best before: ${minFitness(settings.initialPopulation)}")
-  private val after: Int = minFitness(evolved)
-  println(s"Best after: $after")
-  if (after < 1670) println(evolved.minBy(_.fitness))
+  private val evolved: List[Permutation] = Await.result(Future.traverse((1 to 4).toList)(_ => evolve(settings)), Duration.Inf)
+
+  println(s"Best before: ${settings.initialPopulation.best.fitness}")
+
+  evolved.foreach { best =>
+    val f = fitness.value(best)
+    println(s"Best after: $f")
+    if (f < 6950) println(best)
+  }
+
+  //6922 (OPT)    (8,15,16,14,19,6,7,17,1,12,10,11,5,20,2,3,4,9,18,13)
 }
