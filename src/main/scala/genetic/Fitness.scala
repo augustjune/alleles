cb6380a1fda23c245a3e3e5d3d185a07e03d92d4
empi <jurij.jurich@gmail.com>
2018-09-11 13:39:48 +0200

    Doc added to Fitness and RandomChange type classes

--- src/main/scala/genetic/Fitness.scala
+++ src/main/scala/genetic/Fitness.scala
@@ -1,9 +1,17 @@
 package genetic
 
-object Fitness {
-  def apply[A](a: A)(implicit f: Fitness[A]): Int = f.value(a)
+/**
+  * Fitness function for instances of type `A`, which computes
+  * how close a given design solution is to achieving the set aims
+  */
+trait Fitness[-A] {
+  def value(a: A): Int
 }
 
-trait Fitness[A] {
-  def value(a: A): Int
+object Fitness {
+  /**
+    * Applies trait function from the implicit scope;
+    * allows to use trait as context bounds
+    */
+  def apply[A](a: A)(implicit f: Fitness[A]): Int = f.value(a)
 }
--- src/main/scala/genetic/RandomChange.scala
+++ src/main/scala/genetic/RandomChange.scala
@@ -1,9 +1,24 @@
 package genetic
 
-object RandomChange {
-  def apply[A](a: A)(implicit m: RandomChange[A]): A = m.modify(a)
-}
-
+/**
+  * Any function which allows to create a new instance of type `A`
+  * which differs from the original one in random place
+  *
+  * Laws:
+  *   1. Modified instance does not equal to original one
+  *     f(a) != a
+  *   2. After a certain number of modification the same input produces different outputs
+  *     def f5(a: A) = f(f(f(f(f(a)))))
+  *     f5(a) != f5(a)
+  */
 trait RandomChange[A] {
   def modify(a: A): A
+}
+
+object RandomChange {
+  /**
+    * Applies trait function from the implicit scope;
+    * allows to use trait as context bounds
+    */
+  def apply[A](a: A)(implicit m: RandomChange[A]): A = m.modify(a)
 }
