cf8faf94eb544755fc6fadb5d21b0d06dc8ffd85
empi <jurij.jurich@gmail.com>
2018-09-11 16:24:24 +0200

    Small code refactoring; Added extension method to Population

--- src/main/scala/genetic/operators/selection/Tournament.scala
+++ src/main/scala/genetic/operators/selection/Tournament.scala
@@ -7,8 +7,9 @@ import genetic.genotype.Fitness
 import scala.util.Random
 
 case class Tournament[A: Fitness](size: Int) extends Selection[A] {
-  def apply(population: Population[A]): Population[A] = population.map(_ =>
-    Random.shuffle(population).take(size).minBy(Fitness(_)))
+  def apply(population: Population[A]): Population[A] =
+    for (_ <- population) yield
+    Random.shuffle(population).take(size).minBy(Fitness(_))
 
   override def toString: String = s"Tournament selection with tournament size of $size"
 }
