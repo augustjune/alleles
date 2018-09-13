c23f37d651021a2129f663db78c87fffd403b16b
empi <jurij.jurich@gmail.com>
2018-09-13 12:12:45 +0200

    Changed val parameters of value classes to be private

--- src/main/scala/genetic/genotype/syntax.scala
+++ src/main/scala/genetic/genotype/syntax.scala
@@ -3,15 +3,15 @@ package genetic.genotype
 import genetic.Population
 
 object syntax {
-  implicit class FitnessObj[G](val g: G) extends AnyVal {
+  implicit class FitnessObj[G](private val g: G) extends AnyVal {
     def fitness(implicit f: Fitness[G]): Int = f.value(g)
   }
 
-  implicit class RandomChangeObj[G](val g: G) extends AnyVal {
+  implicit class RandomChangeObj[G](private val g: G) extends AnyVal {
     def mutated(implicit c: RandomChange[G]): G = c.modify(g)
   }
 
-  implicit class SchemeObj[G](val g: G) extends AnyVal {
+  implicit class SchemeObj[G](private val g: G) extends AnyVal {
     def populate(n: Int): Population[G] = Scheme.fromOne(g).make(n)
   }
 }
