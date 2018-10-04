c9f1272e6f7026aea6e18226b01c6eaa7b77bcee
empi <jurij.jurich@gmail.com>
2018-10-04 09:45:41 +0200

    Expanded Scheme with additional constructors

--- src/main/scala/genetic/genotype/Scheme.scala
+++ src/main/scala/genetic/genotype/Scheme.scala
@@ -24,6 +24,24 @@ object Scheme {
     def create: G = f()
   }
 
+  /**
+    * Return scheme which creates elements from cycled iterator
+    */
+  def fromIterator[G](iterF: () => Iterator[G]): Scheme[G] = new Scheme[G] {
+    private val iterator: Iterator[G] = Iterator.continually {
+      val i = iterF()
+      if (i.isEmpty) throw new RuntimeException("Passed iterator is empty")
+      else i
+    }.flatten
+
+    def create: G = iterator.next()
+  }
+
+  /**
+    * Return scheme which creates elements from cycled iterable
+    */
+  def fromIterable[G](iterable: Iterable[G]): Scheme[G] = fromIterator(() => iterable.iterator)
+
   def make[G](n: Int)(implicit source: Scheme[G]): Population[G] = source.make(n)
 
   /**
