eeb865342dcc2d3e0219a7892a5ef35131c6295c
empi <jurij.jurich@gmail.com>
2018-10-22 17:21:01 +0200

    Delegated all tree operations to tree subtypes

--- src/main/scala/examples/geneticProgramming/FunTree.scala
+++ src/main/scala/examples/geneticProgramming/FunTree.scala
@@ -2,7 +2,6 @@ package examples.geneticProgramming
 
 import lazyOr._
 
-
 sealed trait FunTree {
   /**
     * Replace this tree or any of its subtree with other tree
@@ -17,90 +16,40 @@ sealed trait FunTree {
     * @return Pair of original trees, where each contains part of one another
     */
   def cross(other: FunTree): (FunTree, FunTree)
-
-  /**
-    * Insert new random tree instead of `this` tree or any of its subtree
-    *
-    * @return
-    */
-  def modify(): FunTree
 }
 
-final case class Variable(name: String) extends FunTree {
-  def insert(other: FunTree): (FunTree, FunTree) = (other, this)
-
-  def cross(other: FunTree): (FunTree, FunTree) = insert(other) >|| other.insert(this).reverse
-
-  def modify(): FunTree = ???
-}
-
-final case class Value(v: Double) extends FunTree {
+sealed abstract class Const extends FunTree {
   def insert(other: FunTree): (FunTree, FunTree) = (other, this)
 
   def cross(other: FunTree): (FunTree, FunTree) = other.insert(this).reverse
-
-  def modify(): FunTree = ???
 }
 
-final case class Sin(a: FunTree) extends FunTree {
-  def insert(other: FunTree): (FunTree, FunTree) = (other, this) >|| (Sin(other), a) >|| a.insert(other).mapFirst(Sin)
+sealed abstract class UnaryOp(op: FunTree => FunTree, a: FunTree) extends FunTree {
+  def insert(other: FunTree): (FunTree, FunTree) = (other, this) >|| (op(other), a) >|| a.insert(other).mapFirst(op)
 
-  def cross(other: FunTree): (FunTree, FunTree) = other.insert(this).reverse >|| other.insert(a).mapSecond(Sin).reverse
-
-  def modify(): FunTree = ???
+  def cross(other: FunTree): (FunTree, FunTree) = other.insert(this).reverse >|| other.insert(a).mapSecond(op).reverse
 }
 
-final case class Cos(a: FunTree) extends FunTree {
-  def insert(other: FunTree): (FunTree, FunTree) = (other, this) >|| (Cos(other), a) >|| a.insert(other).mapFirst(Cos)
-
-  def cross(other: FunTree): (FunTree, FunTree) = other.insert(this).reverse >|| other.insert(a).mapSecond(Cos).reverse
-
-  def modify(): FunTree = ???
-}
-
-final case class Plus(a: FunTree, b: FunTree) extends FunTree {
-  def insert(other: FunTree): (FunTree, FunTree) = (other, this) >|| (Plus(a, other), b) >|| (Plus(other, b), a) >||
-    a.insert(other).mapFirst(Plus(_, b)) >|| b.insert(other).mapFirst(Plus(a, _))
+sealed abstract class BinaryOp(op: (FunTree, FunTree) => FunTree, a: FunTree, b: FunTree) extends FunTree {
+  def insert(other: FunTree): (FunTree, FunTree) = (other, this) >|| (op(a, other), b) >|| (op(other, b), a) >||
+    a.insert(other).mapFirst(op(_, b)) >|| b.insert(other).mapFirst(op(a, _))
 
   def cross(other: FunTree): (FunTree, FunTree) = other.insert(this).reverse >||
-    other.cross(a).mapSecond(Plus(_, b)).reverse >|| other.cross(b).mapSecond(Plus(a, _)).reverse
-
-  def modify(): FunTree = ???
+    other.cross(a).mapSecond(op(_, b)).reverse >|| other.cross(b).mapSecond(op(a, _)).reverse
 }
 
-final case class Minus(a: FunTree, b: FunTree) extends FunTree {
-  def insert(other: FunTree): (FunTree, FunTree) = (other, this) >|| (Minus(a, other), b) >|| (Minus(other, b), a) >||
-    a.insert(other).mapFirst(Minus(_, b)) >|| b.insert(other).mapFirst(Minus(a, _))
+final case class Variable(name: String) extends Const
 
-  def cross(other: FunTree): (FunTree, FunTree) = other.insert(this).reverse >||
-    other.cross(a).mapSecond(Minus(_, b)).reverse >|| other.cross(b).mapSecond(Minus(a, _)).reverse
+final case class Value(v: Double) extends Const
 
-  def modify(): FunTree = ???
-}
+final case class Sin(a: FunTree) extends UnaryOp(Sin, a)
 
-final case class Multiply(a: FunTree, b: FunTree) extends FunTree {
-  def insert(other: FunTree): (FunTree, FunTree) = (other, this) >|| (Multiply(a, other), b) >|| (Multiply(other, b), a) >||
-    a.insert(other).mapFirst(Multiply(_, b)) >|| b.insert(other).mapFirst(Multiply(a, _))
+final case class Cos(a: FunTree) extends UnaryOp(Cos, a)
 
-  def cross(other: FunTree): (FunTree, FunTree) = ???
+final case class Plus(a: FunTree, b: FunTree) extends BinaryOp(Plus, a, b)
 
-  def modify(): FunTree = ???
-}
+final case class Minus(a: FunTree, b: FunTree) extends BinaryOp(Minus, a, b)
 
-final case class Divide(a: FunTree, b: FunTree) extends FunTree {
-  def insert(other: FunTree): (FunTree, FunTree) = (other, this) >|| (Divide(a, other), b) >|| (Divide(other, b), a) >||
-    a.insert(other).mapFirst(Divide(_, b)) >|| b.insert(other).mapFirst(Divide(a, _))
+final case class Multiply(a: FunTree, b: FunTree) extends BinaryOp(Multiply, a, b)
 
-  def cross(other: FunTree): (FunTree, FunTree) = ???
-
-  def modify(): FunTree = ???
-}
-
-/*
-abstract class BinaryOp[T <: (FunTree, FunTree) => FunTree](op: T) {
-  def insert(base: FunTree, other: FunTree)(arg1: FunTree, arg2: FunTree): (FunTree, FunTree) = {
-    (op(other, arg2), arg1) >|| (op(arg1, other), arg2) >||
-  }
-
-  def cross(x: FunTree, y: FunTree)(arg1: FunTree, arg2: FunTree)
-}*/
+final case class Divide(a: FunTree, b: FunTree) extends BinaryOp(Divide, a, b)
--- src/main/scala/examples/geneticProgramming/TreeJoin.scala
+++ src/main/scala/examples/geneticProgramming/TreeJoin.scala
@@ -2,47 +2,10 @@ package examples.geneticProgramming
 
 import genetic.collections.IterablePair
 import genetic.genotype.Join
-import lazyOr._
 
 object TreeJoin extends Join[FunTree] {
 
-  // (new, left)
-  def insert(base: FunTree, other: FunTree): (FunTree, FunTree) = {
-    def insert1(ap: FunTree => FunTree, a: FunTree): (FunTree, FunTree) =
-      (ap(other), a) >|| insert(a, other).mapFirst(ap)
-
-    def insert2(ap: (FunTree, FunTree) => FunTree, a: FunTree, b: FunTree): (FunTree, FunTree) =
-      (ap(other, b), a) >|| (ap(a, other), b) >|| insert(a, other).mapFirst(ap(_, b)) >|| insert(b, other).mapFirst(ap(a, _))
-
-    base match {
-      case Variable(_) | Value(_) => (other, base)
-      case Sin(a) => insert1(Sin, a)
-      case Cos(a) => insert1(Cos, a)
-      case Plus(a, b) => insert2(Plus, a, b)
-      case Minus(a, b) => insert2(Minus, a, b)
-      case Multiply(a, b) => insert2(Multiply, a, b)
-      case Divide(a, b) => insert2(Divide, a, b)
-    }
+  def cross(x: FunTree, y: FunTree): IterablePair[FunTree] = x.cross(y) match {
+    case (xs, ys) => IterablePair(xs, ys)
   }
-
-  def crossPair(x: FunTree, y: FunTree): (FunTree, FunTree) = {
-
-    def crossOne(ap: FunTree => FunTree, a: FunTree) =
-      insert(x, y) >|| crossPair(x, a).mapSecond(ap)
-
-    def crossTwo(ap: (FunTree, FunTree) => FunTree, a: FunTree, b: FunTree) =
-      insert(x, y) >|| crossPair(x, a).mapSecond(ap(_, b)) >|| crossPair(x, b).mapSecond(ap(a, _))
-
-    y match {
-      case Variable(_) | Value(_) => insert(x, y)
-      case Sin(a) => insert(x, y) >|| crossPair(x, a).mapSecond(Sin)// crossOne(Sin, a)
-      case Cos(a) => insert(x, y) >|| crossPair(x, a).mapSecond(Cos)
-      case Plus(a, b) => insert(x,y) >|| crossPair(x, a).mapSecond(Plus(_, a)) >|| crossPair(x, b).mapSecond(Plus(a, _))   // crossTwo(Plus, a, b)
-      case Minus(a, b) => insert(x,y) >|| crossPair(x, a).mapSecond(Minus(_, a)) >|| crossPair(x, b).mapSecond(Minus(a, _))
-      case Multiply(a, b) => insert(x,y) >|| crossPair(x, a).mapSecond(Multiply(_, a)) >|| crossPair(x, b).mapSecond(Multiply(a, _))
-      case Divide(a, b) => insert(x,y) >|| crossPair(x, a).mapSecond(Divide(_, a)) >|| crossPair(x, b).mapSecond(Divide(a, _))
-    }
-  }
-
-  def cross(x: FunTree, y: FunTree): IterablePair[FunTree] = crossPair(x, y).map(IterablePair.apply)
 }
