0ee7a6564cb6918612b2f2e4984bc38c0e92eba8
empi <jurij.jurich@gmail.com>
2018-11-04 17:40:12 +0100

    Added support of asynchronous input (Fitness function)

--- src/main/scala/genetic/rebasedGA/package.scala
+++ src/main/scala/genetic/rebasedGA/package.scala
@@ -1,5 +1,31 @@
 package genetic
 
+import scala.concurrent.Future
+
 package object rebasedGA {
   type PopulationWithBest[G] = (Population[G], (G, Double))
+
+  /*
+  trait Fitness[G] {
+  def value(g: G): Double
+
+  def cached: CachedFitness[G] = new CachedFitness[G](this)
+}
+
+object Fitness {
+  /**
+    * Applies trait function from the implicit scope;
+    * allows to use trait as context bounds
+    */
+  def apply[G](g: G)(implicit f: Fitness[G]): Double = f.value(g)
+}
+   */
+
+  trait AsyncFitness[G] {
+    def value(g: G): Future[Double]
+  }
+
+  object AsyncFitness {
+    def apply[G](g: G)(implicit f: AsyncFitness[G]): Future[Double] = f.value(g)
+  }
 }
