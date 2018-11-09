48bd44dca41613ead1e616a2963a804733920a69
empi <jurij.jurich@gmail.com>
2018-11-10 00:20:41 +0100

    Created abstraction over fitness evaluation technique via Functor cat

--- src/main/scala/genetic/engines/EvolutionEngine.scala
+++ src/main/scala/genetic/engines/EvolutionEngine.scala
@@ -2,6 +2,7 @@ package genetic.engines
 
 import akka.NotUsed
 import akka.stream.scaladsl.Source
+import cats.Functor
 import genetic.engines.async.AsyncEvolutionEngine
 import genetic.engines.bestTracking.BestTrackingEvolutionEngine
 import genetic.genotype.{Fitness, Join, Modification}
@@ -10,12 +11,21 @@ import genetic.{OperatorSet, Population}
 import scala.concurrent.ExecutionContext
 
 trait EvolutionEngine {
+  /**
+    * Functor upon which Fitness value is going to be evaluated for each population,
+    * with standard scala implementation by default
+    */
+  val populationFunctor: Functor[Population] = new Functor[Population] {
+    def map[A, B](fa: Population[A])(f: A => B): Population[B] = fa.map(f)
+  }
+
   def evolve[G: Fitness : Join : Modification](options: EvolutionOptions[G]): Source[Population[G], NotUsed] =
     Source.repeat(()).scan(options.initialPopulation) {
       case (prev, _) => evolutionStep(evalFitnesses(prev), options.operators)
     }
 
-  def evalFitnesses[G: Fitness](population: Population[G]): Population[(G, Double)]
+  def evalFitnesses[G: Fitness](population: Population[G]): Population[(G, Double)] =
+    populationFunctor.map(population)(g => g -> Fitness(g))
 
   def evolutionStep[G: Join : Modification](scoredPop: Population[(G, Double)],
                                             operators: OperatorSet): Population[G]
--- src/main/scala/genetic/engines/SequentialEvolutionEngine.scala
+++ src/main/scala/genetic/engines/SequentialEvolutionEngine.scala
@@ -4,10 +4,6 @@ import genetic.genotype.{Fitness, Join, Modification}
 import genetic.{OperatorSet, Population}
 
 class SequentialEvolutionEngine extends EvolutionEngine {
-
-  def evalFitnesses[G: Fitness](population: Population[G]): Population[(G, Double)] =
-    population.map(g => g -> Fitness(g))
-
   def evolutionStep[G: Join : Modification](scoredPop: Population[(G, Double)],
                                             operators: OperatorSet): Population[G] =
     operators.mutation.generation(
