47b598726c5842a859a401c11e7ebb043f804522
empi <jurij.jurich@gmail.com>
2018-11-23 23:53:09 +0100

    Changed concept of FitnessEvaluator to be class which takes functor logic as a parameter

--- src/main/scala/genetic/engines/FitnessEvaluator.scala
+++ src/main/scala/genetic/engines/FitnessEvaluator.scala
@@ -4,13 +4,7 @@ import cats.Functor
 import genetic.Population
 import genetic.genotype.Fitness
 
-trait FitnessEvaluator {
-  /**
-    * Functor upon which Fitness value is going to be evaluated for each population,
-    * with standard scala implementation by default
-    */
-  protected val populationFunctor: Functor[Population]
-
+class FitnessEvaluator(populationFunctor: Functor[Population]) {
   def rate[G: Fitness](population: Population[G]): Population[Rated[G]] =
     populationFunctor.map(population)(g => g -> Fitness(g))
 }
--- src/main/scala/genetic/engines/sequential/localcached/LocalCacheFitnessEvaluator.scala
+++ src/main/scala/genetic/engines/sequential/localcached/LocalCacheFitnessEvaluator.scala
@@ -4,15 +4,11 @@ import cats.Functor
 import genetic.Population
 import genetic.engines.FitnessEvaluator
 
-trait LocalCacheFitnessEvaluator extends FitnessEvaluator {
-  /**
-    * Evaluating fitness value only for unique values of population,
-    * copying computed result to identical individuals
-    */
-  protected val populationFunctor: Functor[Population] = new Functor[Population] {
+object LocalCacheFitnessEvaluator extends  {
+  private val populationFunctor: Functor[Population] = new Functor[Population] {
     def map[A, B](fa: Population[A])(f: A => B): Population[B] = {
       val cache = fa.map(x => (x, x)).toMap.mapValues(f)
       fa.map(cache)
     }
   }
-}
+} with FitnessEvaluator(populationFunctor)
