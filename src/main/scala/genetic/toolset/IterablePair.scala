65be2081e9bee0ece382c7b058e38a2b3b19293a
Yuriy Slinkin <ext-yuriy.slinkin@here.com>
2019-03-19 11:06:32 +0100

    Provided IterablePair with standard Iterator implementation

--- src/main/scala/genetic/toolset/IterablePair.scala
+++ src/main/scala/genetic/toolset/IterablePair.scala
@@ -7,14 +7,5 @@ import scala.collection.immutable.Iterable
   */
 case class IterablePair[A](l: A, r: A) extends Iterable[A] {
 
-  def iterator: Iterator[A] = new Iterator[A] {
-    private var current = 0
-
-    def hasNext: Boolean = current < 2
-
-    def next(): A = current match {
-      case 0 => current += 1; l
-      case 1 => current += 1; r
-    }
-  }
+  def iterator: Iterator[A] = Iterator(l, r)
 }
