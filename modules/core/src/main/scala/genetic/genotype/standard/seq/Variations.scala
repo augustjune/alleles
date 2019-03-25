eb6db0f11af32f51ec0f3b77b7e0d59adf87f3e5
Yura Slinkin <jurij.jurich@gmail.com>
2019-03-25 23:17:56 +0100

    Polymorphic way to create variation instances for seq-like individuals with type preservation

--- modules/core/src/main/scala/genetic/genotype/standard/seq/Variations.scala
+++ modules/core/src/main/scala/genetic/genotype/standard/seq/Variations.scala
@@ -1,20 +1,25 @@
 package genetic.genotype.standard.seq
 
 import genetic.genotype.Variation
-import genetic.toolset.RRandom
 import genetic.genotype.syntax._
+import genetic.toolset.RRandom
+
+import scala.collection.SeqLike
+import scala.collection.generic.CanBuildFrom
 
 object Variations {
-  def swap[A]: Variation[Seq[A]] =
-    (ind: Seq[A]) => {
+
+  def swap[CC[X] <: SeqLike[X, CC[X]], A](implicit cbf: CanBuildFrom[CC[_], A, CC[A]]): Variation[CC[A]] =
+    (ind: CC[A]) => {
       // ToDo - handle same indices and individuals of size < 2
       val i1, i2 = RRandom.nextInt(ind.length)
       val temp = ind(i1)
       ind.updated(i1, ind(i2)).updated(i2, temp)
     }
 
-  def elementVariation[A: Variation]: Variation[Seq[A]] =
-    (ind: Seq[A]) => {
+  def elementVariation[CC[X] <: SeqLike[X, CC[X]], A]
+  (implicit cbf: CanBuildFrom[CC[_], A, CC[A]], v: Variation[A]): Variation[CC[A]] =
+    (ind: CC[A]) => {
       val i = RRandom.nextInt(ind.length)
       ind.updated(i, ind(i).modified)
     }
