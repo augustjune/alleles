855b93fb9db73b56fb39cd9cdd91afdd568e7f55
Yura Slinkin <jurij.jurich@gmail.com>
2019-03-26 10:52:05 +0100

    Handled case with same indices and small individuals in swap variation

--- modules/core/src/main/scala/genetic/genotype/standard/seq/Variations.scala
+++ modules/core/src/main/scala/genetic/genotype/standard/seq/Variations.scala
@@ -9,13 +9,18 @@ import scala.collection.generic.CanBuildFrom
 
 object Variations {
 
+  /**
+    * Makes sense only for individuals of size greater than 1
+    */
   def swap[CC[X] <: SeqLike[X, CC[X]], A](implicit cbf: CanBuildFrom[CC[_], A, CC[A]]): Variation[CC[A]] =
-    (ind: CC[A]) => {
-      // ToDo - handle same indices and individuals of size < 2
-      val i1, i2 = RRandom.nextInt(ind.length)
-      val temp = ind(i1)
-      ind.updated(i1, ind(i2)).updated(i2, temp)
-    }
+    (ind: CC[A]) =>
+      if (ind.size < 2) ind
+      else {
+        val i1 = RRandom.nextInt(ind.length)
+        var i2 = RRandom.nextInt(ind.length - 1)
+        if (i1 == i2) i2 = ind.length - 1
+        ind.updated(i1, ind(i2)).updated(i2, ind(i1))
+      }
 
   def elementVariation[CC[X] <: SeqLike[X, CC[X]], A]
   (implicit cbf: CanBuildFrom[CC[_], A, CC[A]], v: Variation[A]): Variation[CC[A]] =
