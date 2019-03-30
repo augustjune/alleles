4ce32a06a1924a6be505d7c12ad38c16fc2be815
Yura Slinkin <jurij.jurich@gmail.com>
2019-03-30 14:51:18 +0100

    Moved Selection definition to separate file

---
+++ modules/core/src/main/scala/alleles/operators/Selection.scala
@@ -0,0 +1,14 @@
+package alleles.operators
+
+import alleles.Population
+import alleles.genotype.Fitness.Rated
+
+/**
+  * Genetic operator used to choose individual
+  * genomes from a population for later breeding
+  */
+trait Selection {
+  def pair[A](population: Population[Rated[A]]): (A, A)
+
+  def generation[A](population: Population[Rated[A]]): Population[(A, A)]
+}
