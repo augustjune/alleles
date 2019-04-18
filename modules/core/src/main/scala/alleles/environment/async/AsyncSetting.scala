285c8d0a64bd71ac1cd8857bc0f6d26fbd5aa10f
Yura Slinkin <jurij.jurich@gmail.com>
2019-04-18 20:00:26 +0200

    Merged AsyncFitness to subtype of Ambience

--- modules/core/src/main/scala/alleles/environment/async/AsyncSetting.scala
+++ modules/core/src/main/scala/alleles/environment/async/AsyncSetting.scala
@@ -1,9 +1,9 @@
 package alleles.environment.async
 
 import akka.stream.scaladsl.Source
-import alleles.environment.{EvolutionFlow, Progress}
+import alleles.Population
+import alleles.environment.{Ambience, Epic, EvolutionFlow, Progress}
 import alleles.genotype.{Join, Variation}
-import alleles.{Epoch, Population}
 
 import scala.concurrent.{ExecutionContext, Future}
 
@@ -11,16 +11,19 @@ import scala.concurrent.{ExecutionContext, Future}
   * Implementation of genetic algorithm with asynchronous fitness value computation and
   * parametrized of applying genetic operators to populations
   */
-class AsyncSetting(flow: Progress)(implicit executionContext: ExecutionContext) {
-  def evolve[A: AsyncFitness : Join : Variation](initial: Population[A],
-                                                 operators: Epoch[A]): EvolutionFlow[Population[A]] = {
-    Source.repeat(()).scanAsync(initial) {
+class AsyncSetting[A](flow: Progress)(implicit executionContext: ExecutionContext,
+                                      F: AsyncFitness[A],
+                                      J: Join[A],
+                                      V: Variation[A]) extends Ambience[A] {
+
+  def evolve(epic: Epic[A]): EvolutionFlow[Population[A]] = {
+    Source.repeat(()).scanAsync(epic.initialPopulation) {
       case (prev, _) => Future.traverse(prev) { g =>
         for {
           individual <- Future.successful(g)
           fitness <- AsyncFitness(g)
         } yield (individual, fitness)
-      }.map(flow.nextGeneration(_, operators))
+      }.map(flow.nextGeneration(_, epic.operators))
     }
   }
 }
