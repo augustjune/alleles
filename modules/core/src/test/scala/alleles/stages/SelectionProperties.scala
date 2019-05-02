e2e0727b91b41e1df8a7beb6326902df5b970325
Yura Slinkin <jurij.jurich@gmail.com>
2019-05-02 18:06:12 +0200

    Moved common implementation to abstract selection properties definition

--- modules/core/src/test/scala/alleles/stages/SelectionProperties.scala
+++ modules/core/src/test/scala/alleles/stages/SelectionProperties.scala
@@ -1,20 +1,24 @@
 package alleles.stages
 
 import alleles.Population
+import org.scalacheck.Gen.{listOfN, posNum}
 import org.scalacheck.Prop._
 import org.scalacheck.{Gen, Prop, Properties}
 
 abstract class SelectionProperties(name: String) extends Properties(name + " with Selection props") {
-  type Ind
+  type Ind = String
 
-  def populationGen: Gen[Population[(Ind, Double)]]
+  val populationGen: Gen[Population[(Ind, Double)]] = for {
+    n <- posNum[Int]
+    pop <- listOfN(n, Gen.alphaNumStr)
+  } yield pop.map(x => (x, x.length.toDouble)).toVector
 
   def implGen: Gen[Selection]
 
   property("Individuals are selected from the population") = forAll(implGen, populationGen) {
     (implementation, pop) =>
       val (i1, i2) = implementation.pair(pop)
-      val individuals  = pop.map(_._1)
+      val individuals = pop.map(_._1)
       individuals.contains(i1) && individuals.contains(i2)
   }
 
--- modules/core/src/test/scala/alleles/stages/selection/TournamentProperties.scala
+++ modules/core/src/test/scala/alleles/stages/selection/TournamentProperties.scala
@@ -3,33 +3,20 @@ package alleles.stages.selection
 import alleles.genotype.Fitness
 import alleles.genotype.syntax._
 import alleles.stages.{Selection, SelectionProperties}
-import alleles.{Population, PopulationExtension}
-import org.scalacheck.Arbitrary._
 import org.scalacheck.Gen
 import org.scalacheck.Gen._
 import org.scalacheck.Prop._
 
 object TournamentProperties extends SelectionProperties("Tournament props") {
-  type Ind = Int
   val implGen: Gen[Selection] = posNum[Int].map(Selection.tournament(_))
-  val populationGen: Gen[Population[(Ind, Double)]] = for {
-    n <- posNum[Int]
-    pop <- listOfN(n, arbitrary[Int])
-  } yield pop.toVector.withFitnesses
 
-  implicit val fitness: Fitness[Ind] = identity[Ind]
+  implicit val fitness: Fitness[Ind] = _.length.toDouble
 
-  property("Selected individuals are best out of the round") = forAll(populationGen, posNum[Int]) {
-    (pop, roundSize) =>
+  property("Selected individuals are best out of the round") =
+    forAll(populationGen, posNum[Int]) { (pop, roundSize) =>
       val tournament = Selection.tournament(roundSize)
       val (g1, g2) = tournament.pair(pop)
       val bestOfRound = pop.map(_._2).sorted.takeRight(roundSize).head
       g1.fitness <= bestOfRound && g2.fitness <= bestOfRound
-  }
-
-  property("If round transcends population, selected individuals are the same") = forAll(populationGen) { pop =>
-    val tournament = Selection.tournament(pop.size + 1)
-    val (g1, g2) = tournament.pair(pop)
-    g1 == g2
-  }
+    }
 }
