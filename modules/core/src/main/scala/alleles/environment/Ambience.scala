1d354f8aded8ca3574806d787670a887bad4af17
Yura Slinkin <jurij.jurich@gmail.com>
2019-11-20 10:28:26 +0100

    inlined stream declarations

--- modules/core/src/main/scala/alleles/environment/Ambience.scala
+++ modules/core/src/main/scala/alleles/environment/Ambience.scala
@@ -1,11 +1,12 @@
 package alleles.environment
 
 import alleles.Population
+import fs2.{Stream, Pure}
 
 /**
   * Representation of the environment, allowing the population of arbitrary individuals
   * to evolve within a set of genetic operators in a stream of adjusting populations
   */
 trait Ambience[A] {
-  def evolve(epic: Epic[A]): EvolutionFlow[Population[A]]
+  def evolve(epic: Epic[A]): Stream[Pure, Population[A]]
 }
